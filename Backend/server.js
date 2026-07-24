a2841e39ec35098d33674b5b434093883d14bc02
nayandangeSDE27 <163569246+nayandangeSDE27@users.noreply.github.com>
2026-07-24 17:58:09 +0530

    Set default port for the server

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 
 connectToDB()
-
+const PORT = process.env.PORT || 3005
 
 app.listen(PORT, () => {
     console.log("Server is running on port ${PORT}")
