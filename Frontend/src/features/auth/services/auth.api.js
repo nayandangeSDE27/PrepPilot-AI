2c8d9b149a6f29c5e02eb635c305f15d1f5afce4
nayandangeSDE27 <163569246+nayandangeSDE27@users.noreply.github.com>
2026-07-24 18:02:26 +0530

    Change API base URL to production server

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://preppilot-ai-qqgq.onrender.com",
     withCredentials: true
 })
 
